cb4f25f36eaaff22a75941b5ec21dde561c8dbfb
Theo Chatzimichos <tampakrap@gentoo.org>
2013-11-05 00:56:35 +0100

    hardcode the default gentoo package name

--- manifests/package.pp
+++ manifests/package.pp
@@ -3,7 +3,7 @@ class syslogng::package {
   include syslogng
 
   if $::operatingsystem == 'gentoo' {
-    portage::package { $syslogng::package_name:
+    portage::package { 'app-admin/syslog-ng':
       keywords => $syslogng::gentoo_keywords,
       use      => $syslogng::gentoo_use,
       ensure   => $syslogng::ensure,
